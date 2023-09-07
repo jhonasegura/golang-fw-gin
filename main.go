7a815b004ee6d8b91f952aad963cb589e5bf6e47
Jhonathan Segura <jsegura-ext@skillz.com>
2023-09-07 15:30:47 -0500

    Adding first funcionality win GET and POST

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type album struct {
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Artist string `json:"artist"`
+	Year   int    `json:"year"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Familia", Artist: "Camila Cabello", Year: 2022},
+	{ID: "2", Title: "21", Artist: "Adele", Year: 2011},
+	{ID: "3", Title: "The Eminem Show", Artist: "Eminem", Year: 2022},
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+
+	c.IndentedJSON(http.StatusCreated, albums)
+}
+
+func getAlbumsByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Album not found"})
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
+	router.GET("/albums/:id", getAlbumsByID)
+
+	router.Run("localhost:8080")
+}
